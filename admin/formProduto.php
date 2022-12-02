28963cf80578d78501f7ba0525bf518494fc65e2
Gustavo Januário <gustavo.sjanuario@gmail.com>
2022-12-02 11:34:27 -0300

    Update formProduto.php

--- admin/formProduto.php
+++ admin/formProduto.php
@@ -1,7 +1,7 @@
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-    <li class="breadcrumb-item"><a href="index.php?pagina=listaCategoria">Produtos</a></li>
+    <li class="breadcrumb-item"><a href="index.php?pagina=listaProduto">Produtos</a></li>
     <li class="breadcrumb-item active" aria-current="page">Cadastro Produto</li>
   </ol>
 </nav>
